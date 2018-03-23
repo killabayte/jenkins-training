b48a13078524f3b42cfde1c040d0c189bbe7e4fb
ykostiannikov <Yaroslav.Kostiannikov@lookout.com>
2018-03-23 13:01:53 +0200

    update

--- jenkins-builder/scripts/install.sh
+++ jenkins-builder/scripts/install.sh
@@ -11,5 +11,4 @@ cp /root/.ssh/authorized_keys /var/jenkins_home/.ssh/authorized_keys
 chmod 700 /var/jenkins_home/.ssh
 chmod 600 /var/jenkins_home/.ssh/authorized_keys
 chown -R 1000:1000 /var/jenkins_home
-docker run -p 2222:22 -v /var/jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock --restart always -d yaroslavkostyannikov/jenkins-builder
-
+docker run -p 2244:22 -v /var/jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock --restart always -d yaroslavkostyannikov/jenkins-builder
